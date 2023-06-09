da525941a2513481cfadac9e78bdb42f04446a1c
alfdhaill <muh.alfudhail@outlook.com>
2023-06-09 19:18:15 +0700

    docs: update apk

--- Final/app/src/main/java/com/example/afinal/MoviesFragment.java
+++ Final/app/src/main/java/com/example/afinal/MoviesFragment.java
@@ -12,14 +12,14 @@ import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.SearchView;
+import androidx.appcompat.widget.SearchView;
+import androidx.core.view.MenuItemCompat;
 
-import com.example.afinal.MainActivity;
-import com.example.afinal.R;
 import com.example.afinal.adapters.MoviesAdapter;
 import com.example.afinal.apis.ApiConfig;
 import com.example.afinal.models.MoviesResponse;
@@ -97,4 +97,30 @@ public class MoviesFragment extends Fragment {
             }
         });
     }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.search_menu, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setQueryHint("Search now playing movies");
+
+        // Set the query listener
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // Handle query submission
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // Handle query text change
+                return true;
+            }
+        });
+
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
 }
